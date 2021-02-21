69dbb1dd53f40233d39c4146a3f370fe59580a46
Amit Sangwan <a.s.@Amits-MacBook-Pro.local>
2021-02-22 03:05:16 +0530

    somechanges

--- ApiAutomation/src/main/java/base/BaseTest.java
+++ ApiAutomation/src/main/java/base/BaseTest.java
@@ -9,9 +9,14 @@ public class BaseTest {
 
     public Properties properties = new Properties();
 
+    //Not hardcoding status code in @Tests
+    public int RESPONSE_STATUS_CODE_200 =200;
+    public int RESPONSE_STATUS_CODE_500 =500;
+    public int RESPONSE_STATUS_CODE_400 =400;
+    public int RESPONSE_STATUS_CODE_401 =401;
+
     //Base Class Constructor
     public BaseTest() {
-
         {
             try {
                 FileInputStream fInput = new FileInputStream("src/main/java/config/config.properties");
--- ApiAutomation/src/test/java/apiTests/GetApiTest.java
+++ ApiAutomation/src/test/java/apiTests/GetApiTest.java
@@ -2,17 +2,28 @@ package apiTests;
 
 import base.BaseTest;
 import demoClient.RestClient;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import org.apache.http.Header;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 public class GetApiTest extends BaseTest {
 
     BaseTest baseTest;
-    String baseUrl;
-    String apiUrl;
+    String baseUrl = properties.getProperty("baseUrl");
+    String apiUrl = properties.getProperty("apiUrl");
     String url;
+    CloseableHttpResponse closeableHttpResponse;
 
     //Initiallizing BaseTest Class Construction
     @BeforeMethod
@@ -20,12 +31,46 @@ public class GetApiTest extends BaseTest {
         baseTest = new BaseTest();
     }
 
+    /*
+    Using RestAssured
+    REST Assured is a Java library that provides a domain-specific language (DSL)
+    for writing powerful, maintainable tests for RESTful APIs.
+    */
+    @Test
+    public void restAssuredWay() {
+        Response response = RestAssured.given()
+                .spec(new RequestSpecBuilder().setRelaxedHTTPSValidation().setBaseUri(baseUrl)
+                .addHeader("Content-Type", "aapplication/json")
+                .build())
+                .get(apiUrl);
+
+        System.out.println(response.getBody().asString());
+    }
+
+    //Using Apache HTTP Client
     @Test
     public void getApiTest() throws IOException {
-        url = properties.getProperty("baseUrl") + properties.getProperty("apiUrl");
-
+        url = baseUrl + apiUrl;
         RestClient restClient = new RestClient();
-        restClient.get(url);
+        closeableHttpResponse = restClient.get(url);
+
+        //Status Code
+        int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
+
+        Assert.assertEquals(statusCode, RESPONSE_STATUS_CODE_200, "Status Code Assertion Failed");
+
+        //Json Response
+        String responseDataString = EntityUtils.toString(closeableHttpResponse.getEntity(), StandardCharsets.UTF_8);
+        JSONObject jsonObject = new JSONObject(responseDataString);
+
+        //All Headers
+        Header[] headers = closeableHttpResponse.getAllHeaders();
+        HashMap<String, String> allHeaders = new HashMap<>();
+
+        for (Header header : headers) {
+            allHeaders.put(header.getName(), header.getValue());
+        }
+
     }
 
 }
